19076400074409e6eb06fc8a9ebc16bdcc22b58d
Jackson Gray <nohaxjustdoge@gmail.com>
2024-11-20 10:49:26 +0000

    feat: Add file upload form with POST action handler

--- app/routes/upload._index.tsx
+++ app/routes/upload._index.tsx
@@ -1,7 +1,25 @@
-import {Outlet} from "@remix-run/react";
+import type { ActionFunction, LoaderFunction } from "@remix-run/cloudflare";
+import { Form } from "@remix-run/react";
 
+export const loader: LoaderFunction = async ({ request, context }) => {
+  return 0;
+};
 
-export default function Upload_index() {
-  return <div>upload page
-  <Outlet/></div>
+export const action: ActionFunction = async ({ request, context }) => {
+  const url = new URL(request.url);
+  const formData = await request.formData();
+  console.log("action function called")
+  return 0;
+};
+
+export default function Upload() {
+  return (
+    <div className={"h-screen w-screen bg-blue-950"}>
+      upload page
+      <Form method={"POST"}>
+        <input type={"file"}></input>
+        <button type={"submit"}>SUBMIT</button>
+      </Form>
+    </div>
+  );
 }
